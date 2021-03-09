1b3cf544e50cbfc2b7db41619663ded0424c9e63
srebughini <ste.rebu@gmail.com>
2021-03-09 20:41:13 +0100

    New images background

--- js/beerq.js
+++ js/beerq.js
@@ -4,6 +4,6 @@ window.getQuote = function getQuote()
 {
   let beerquotes = BeerQuote("eng");
   let randomQuote = beerquotes.getRandomQuote();
-  document.getElementById("quote").innerHTML = "<i>" + randomQuote.quote + "<i>";
+  document.getElementById("quote").innerHTML = "<i>" + randomQuote.quote + "</i>" ;
   document.getElementById("author").innerHTML = randomQuote.author;
 }
